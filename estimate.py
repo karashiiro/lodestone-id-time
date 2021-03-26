96a1688ddaee45f8fb5b9c915654b1d779398681
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-26 13:13:24 -0700

    Update objective function

--- estimate.py
+++ estimate.py
@@ -5,13 +5,15 @@ from scipy.optimize import curve_fit
 
 def objective(x, m, b1, a, b2):
     if np.all(x <= 5000000):
-        return 37.44 / 5000000 + 41539.9318171296
+        return 37.44 / 5000000 + 41539.93
+    if np.all(x > 28208601):
+        return 305 / 4775200 * x + 42030.57
     return b1 + m * x + a * sin(b2 * x)
 
 def main():
     # characters_cvt.csv just has the created_at column converted into float form
     df = pd.read_csv("characters_cvt.csv")
-    df = df[df["id"] > 5000000]
+    df = df[(df["id"] > 5000000) & (df["id"] <= 28208601)]
     popt, _ = curve_fit(
         objective,
         df["id"].to_numpy(),
