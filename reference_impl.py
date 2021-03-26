04d41aa4dbd16d7d870005312b3f19977f3fc4a6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-26 16:30:03 -0700

    Add comments to reference implementation

--- reference_impl.py
+++ reference_impl.py
@@ -2,6 +2,10 @@ from datetime import datetime
 from math import sin, pow
 
 def lodestone_id_time(id):
+    """
+    Calculates the creation time of a FFXIV character,
+    and returns it as a datetime object.
+    """
     if id <= 5000000:
         excel_time = 37.44 / 5000000 * id + 41539.93
     elif id > 28208601:
@@ -10,5 +14,5 @@ def lodestone_id_time(id):
         excel_time = 4.10315437 * pow(10, 4) \
         + 1.00993557 * pow(10, -4) * id \
         + 31.5417054 * sin(8.57105764 * pow(10, -7) * id)
-    unix_time = (excel_time - 25569) * 86400
+    unix_time = (excel_time - 25569) * 86400 # Seconds
     return datetime.fromtimestamp(unix_time)
