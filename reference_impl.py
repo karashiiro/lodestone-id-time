a4562291bc25d16fe7d8874082a064ea04577c8a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-26 16:16:23 -0700

    Fix reference implementation

--- reference_impl.py
+++ reference_impl.py
@@ -3,10 +3,11 @@ from math import sin, pow
 
 def lodestone_id_time(id):
     if id <= 5000000:
-        return 37.44 / 5000000 + 41539.93
-    if id > 28208601:
-        return 305.01 / 4775200 * id + 42030.57
-    excel_time = 4.10315437 * pow(10, 4) \
+        excel_time = 37.44 / 5000000 + 41539.93
+    elif id > 28208601:
+        excel_time = 305.01 / 4775200 * id + 42030.57
+    else:
+        excel_time = 4.10315437 * pow(10, 4) \
         + 1.00993557 * pow(10, -4) * id \
         + 31.5417054 * sin(8.57105764 * pow(10, -7) * id)
     unix_time = (excel_time - 25569) * 86400
